d7e84554bcfe974e32c744fea362deacbc5aadd0
hans rijk <hans-rijk@hotmail.com>
2017-12-14 21:05:20 +0100

    Full script so far

---
+++ phoneAndEmail.py
@@ -0,0 +1,17 @@
+#! python 3
+
+import re, pyperclip
+
+# TODO: Create a regex for phone numbers
+re.compile(r'''
+# 06-27369384, 0627369384, 06(-)27369384, 0320-288215, 038-1234567
+
+''',re.VERBOSE)
+
+# TODO: Create a regex for email addresses
+
+# TODO: Get the text off the clipboard
+
+# TODO: Extract the email/phone from this text
+
+# TODO: Copy the extracted email/phone to the clipboard
