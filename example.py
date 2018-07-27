8c2be4b7e16c69786330e9d21879e2044291edb0
hans rijk <hans-rijk@hotmail.com>
2018-07-27 22:17:04 +0200

    change all

--- example.py
+++ example.py
@@ -1,25 +1,10 @@
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+import docx
 
-#to log to new txt file in rel. path:
-#logging.basicConfig(filename='myProgramLog.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+def getText(filename):
+    doc = docx.Document(filename)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
 
-#to disable the logging when done:
-#logging.disable(logging.CRITICAL)
-
-
-logging.debug('Start of program')
-
-def factorial(n):
-    logging.debug('Start of factorial(%s)' % (n))
-    total = 1
-    for i in range(1, n + 1):
-        total *= i
-        logging.debug('i is %s, total is %s' % (i, total))
-
-    logging.debug('Return value is %s' %(total))
-    return total
-
-print(factorial(5))
-
-logging.debug('End of program')
+print(getText('/home/hans/Downloads/demo.docx'))
